0d0a270a912bf13ff5e179a99c4e18305915b953
a.nikitina <nikitina_anna08@list.ru>
2022-12-19 17:46:39 +0300

    Change classes

--- main.py
+++ main.py
@@ -5,29 +5,24 @@ from datetime import datetime
 import mimetypes
 import os
 from tqdm import tqdm
-from settings import TOKEN_VK
+import configparser
+config = configparser.ConfigParser()
+config.read("settings.ini")
 
-access_token = TOKEN_VK
-
-class Uploader:
-
-    base_host = 'https://cloud-api.yandex.net/'
+class API_VK:
 
     def __init__(self, version='5.131'):
-        self.token = access_token
+        self.token = config["VK"]["TOKEN_VK"]
         self.id = user_id
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
         self.name_photos = {}
         self.date = {}
-        self.token_disk = token_disk
 
 
     def _get_photos_info(self):
-
         """ Получает список фотографий из профиля пользователя ВКонтакте
        с копией изображения z размера"""
-
         base_url = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': self.id,
                   'album_id': 'profile',
@@ -44,8 +39,12 @@ class Uploader:
             else:
                 self.name_photos[url] = count_likes, datetime.fromtimestamp(self.date[url]).strftime('%Y-%m-%d')
         print(len(self.name_photos))
-        pprint(dict_photos)
+        pprint(self.name_photos)
         return
+
+class API_YA:
+    def __init__(self, token_disk):
+        self.token_disk = token_disk
 
     def _get_headers(self):
         return {
@@ -53,11 +52,10 @@ class Uploader:
             'Authorization': f'OAuth {self.token_disk}'
         }
 
-    def create_folder(self):
-
+    def _create_folder(self):
         """Создает папку с именем folder_name на Яндекс-диске"""
-
-        folder_url = self.base_host + 'v1/disk/resources'
+        # base_host = 'https://cloud-api.yandex.net'
+        folder_url = 'https://cloud-api.yandex.net/v1/disk/resources'
         params = {'path': folder_name}
         folder = requests.put(folder_url, headers=self._get_headers(),
                                        params=params)
@@ -66,38 +64,39 @@ class Uploader:
             if answer == 'y':
                 return True
         else:
-            print(folder)
+            print(folder.status_code)
         return
 
-
-    def upload(self, user_id, num_photos):
+    def upload(self, user_id, num_photos, name_photos):
         """Загружает фотографии из списка в созданную папку"""
-        url_upload = self.base_host + 'v1/disk/resources/upload'
+        url_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         num_photo = -1
-        for key, value in tqdm(self.name_photos.items(), ncols=100,
+        for key, value in tqdm(name_photos.items(), ncols=100,
                            dynamic_ncols=True, desc='Loading'):
             num_photo += 1
             if num_photo == num_photos:
                 break
-            params = {'path': f"{folder_name}/{value}", 'url':key,
+            params = {'path': f'{folder_name}/{value}', 'url':key,
                       'disable_redirects': True}
             response_upload = requests.post(url_upload,
-                                        headers=self._get_headers(),
-                                        params=params)
+                                            headers=self._get_headers(),
+                                            params=params)
             if response_upload.status_code == 202:
                 print('Загрузка прошла успешно!')
             else:
                 print(f'Ошибка: {response_upload.status_code}')
 
-            list_info_photo = []
-            with open('photos_info.json', 'w') as file:
-                for key, value in self.name_photos.items():
-                    list_info_photo.append({
-                            'file_name': value,
-                             'size': 'z'
-                            }
-                        )
-                json.dump(list_info_photo, file)
+        list_info_photo = []
+        with open('photos_info.json', 'w') as file:
+            for key, value in vk.name_photos.items():
+                list_info_photo.append(
+                   {
+                       'file_name': value,
+                       'size': 'z'
+                   }
+                )
+
+            json.dump(list_info_photo, file)
 
 
 if __name__ == '__main__':
@@ -105,12 +104,14 @@ if __name__ == '__main__':
     user_id = int(input('Введите id пользователя: '))
     token_disk = input('Введите токен Яндекс диска: ')
     folder_name = input("Введите название папки, в которой хотите хранить фото БЕЗ ПРОБЕЛОВ: ")
-    uploader = Uploader()
-    num_photos = int(input(f'Введите количество фото из {len(uploader.name_photos)}, которое хотите '
+    vk = API_VK()
+    ya = API_YA(token_disk)
+    num_photos = int(input(f'Введите количество фото из {len(vk.name_photos)}, которое хотите '
                        f'загрузить'))
-    uploader._get_photos_info()
-    uploader.create_folder()
-    uploader.upload(user_id, num_photos)
+    vk._get_photos_info()
+    # pprint(vk.name_photos)
+    ya._create_folder()
+    ya.upload(user_id, num_photos, vk.name_photos)
 
 
 
